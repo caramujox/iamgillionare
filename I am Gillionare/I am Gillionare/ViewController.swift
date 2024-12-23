f51ee5c1fb318b96d726a8875e1cade4cb4c4e63
Caio-MacBook <caioa.morais@gmail.com>
2024-12-23 02:29:54 -0300

    [feat] i am gillionaree

---
+++ I am Gillionare/I am Gillionare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Gillionare
+//
+//  Created by Caio Araujo on 23/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I am Gillionare/I am GillionareTests/I_am_GillionareTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_GillionareTests.swift
+//  I am GillionareTests
+//
+//  Created by Caio Araujo on 23/12/24.
+//
+
+import Testing
+@testable import I_am_Gillionare
+
+struct I_am_GillionareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
